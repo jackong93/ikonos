f734cffe648eabf10aa3fff02050081054f2e69b
Jack Ong <jack.ong@thecarousell.com>
2018-02-07 09:52:16 +0800

    Fix import

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:ikonos
+web: gunicorn run:app
--- ikonos/__init__.py
+++ ikonos/__init__.py
@@ -8,3 +8,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from ikonos import views, models
+
+if __name__ == '__main__':
+    app.run()
